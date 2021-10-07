89cef6eb7b7e55c80ff531ef16106ab6bd05e38d
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-07 13:29:36 +0600

    modified signIn.test and signOut.test

--- tests/users/signIn.test.js
+++ tests/users/signIn.test.js
@@ -2,7 +2,7 @@ const api = require('../../api');
 const env = require('../../config/config');
 const validate = require('jsonschema').validate;
 
-test('POST /users/signIn',async()=>{
+test('POST/users/signIn',async()=>{
     let response = await api.handlePOSTSignIn({phone:"8801521438557",password: env.MAHATHIR_PASSWORD});
     //response.body.taaha=true
     let validationResult = validate(response.data, {
--- tests/users/signOut.test.js
+++ tests/users/signOut.test.js
@@ -2,7 +2,7 @@ const api = require('../../api');
 const env = require('../../config/config');
 const validate = require('jsonschema').validate;
 
-test('DELETE /users/signOut',async()=>{
+test('DELETE/users/signOut',async()=>{
     let user = await api.handlePOSTSignIn({phone:"8801521438557",password: env.MAHATHIR_PASSWORD});
     let response = await api.handleDELETESignOut(user.data.token)
     let validationResult = validate(response.data, {
