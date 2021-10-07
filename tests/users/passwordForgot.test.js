387a153f73620ab18cd3577886da6957e9978bbf
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-07 18:57:51 +0600

    test case done for passwordForgot

---
+++ tests/users/passwordForgot.test.js
@@ -0,0 +1,16 @@
+const api = require('../../api');
+const validate = require('jsonschema').validate;
+
+test('POST/users/password/forgot',async()=>{
+    let response = await api.handlePOSTPasswordForgot({phone:"8801521438557"});
+    let validationResult = validate(response.data, {
+        "type": "object",
+        "additionalProperties":false,
+        "properties": {
+            "status":{"type":"string"},
+            "statusCode": { "const": 200},
+            "message":{"type":"string"}
+        },
+    });
+    expect(validationResult.errors).toEqual([]);
+})
