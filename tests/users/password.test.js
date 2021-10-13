77bd68b8317509e457034065b5bdf5512c50d0e9
Mahathir <mirmahathir1@gmail.com>
2021-10-13 20:17:09 +0600

    completed /users/ route without redirection urls

--- tests/users/password.test.js
+++ tests/users/password.test.js
@@ -1,32 +1,37 @@
-const api = require('../../api');
+const {badhanAxios} = require('../../api');
 const validate = require('jsonschema').validate;
 const env = require('../../config/config');
+const {processError}=require('../fixtures/helpers');
 
 test('PATCH/users/password', async () => {
-    let signInResponse = await api.badhanAxios.post('/users/signin', {
-        phone: "8801521438557",
-        password: env.MAHATHIR_PASSWORD
-    });
-    let passwordResponse = await api.badhanAxios.patch("/users/password", {"password": env.MAHATHIR_PASSWORD}, {
-        headers: {
-            "x-auth": signInResponse.data.token
-        }
-    });
-    let validationResult = validate(passwordResponse.data, {
-        "type": "object",
-        "additionalProperties": false,
-        "properties": {
-            "status": {"type": "string"},
-            "statusCode": {"const": 201},
-            "message": {"type": "string"},
-            "token": {"type": "string"}
-        },
-        "required": ["status", "statusCode", "token", "message"]
-    });
-    expect(validationResult.errors).toEqual([]);
-    await api.badhanAxios.delete('/users/signout', {
-        headers: {
-            "x-auth": passwordResponse.data.token
-        }
-    });
+    try {
+        let signInResponse = await badhanAxios.post('/users/signin', {
+            phone: "8801521438557",
+            password: env.MAHATHIR_PASSWORD
+        });
+        let passwordResponse = await badhanAxios.patch("/users/password", {"password": env.MAHATHIR_PASSWORD}, {
+            headers: {
+                "x-auth": signInResponse.data.token
+            }
+        });
+        let validationResult = validate(passwordResponse.data, {
+            "type": "object",
+            "additionalProperties": false,
+            "properties": {
+                "status": {"type": "string"},
+                "statusCode": {"const": 201},
+                "message": {"type": "string"},
+                "token": {"type": "string"}
+            },
+            "required": ["status", "statusCode", "token", "message"]
+        });
+        expect(validationResult.errors).toEqual([]);
+        await badhanAxios.delete('/users/signout', {
+            headers: {
+                "x-auth": passwordResponse.data.token
+            }
+        });
+    }catch (e) {
+        throw processError(e);
+    }
 })
--- tests/users/signOut.test.js
+++ tests/users/signOut.test.js
@@ -1,28 +1,27 @@
-const api = require('../../api');
+const {badhanAxios} = require('../../api');
 const validate = require('jsonschema').validate;
 const env = require('../../config/config');
-
+const {processError}=require('../fixtures/helpers')
 test('DELETE/users/signOut', async () => {
-
-    let signInResponse = await api.badhanAxios.post('/users/signin', {
-        phone: "8801521438557",
-        password: env.MAHATHIR_PASSWORD
-    });
-    let signOutResponse = await api.badhanAxios.delete('/users/signout', {
-        headers: {
-            "x-auth": signInResponse.data.token
-        }
-    });
-    let validationResult = validate(signOutResponse.data, {
-        "type": "object",
-        "additionalProperties": false,
-        "properties": {
-            "status": {"type": "string"},
-            "statusCode": {"const": 200},
-            "message": {"type": "string"}
-        },
-        "required": ["status", "statusCode", "message"]
-    });
-    expect(validationResult.errors).toEqual([]);
-
+    try {
+        let signInResponse = await badhanAxios.post('/users/signIn',{phone:"8801521438557",password: env.MAHATHIR_PASSWORD});
+        let signOutResponse = await badhanAxios.delete('/users/signout', {
+            headers: {
+                "x-auth": signInResponse.data.token
+            }
+        });
+        let validationResult = validate(signOutResponse.data, {
+            "type": "object",
+            "additionalProperties": false,
+            "properties": {
+                "status": {"type": "string"},
+                "statusCode": {"const": 200},
+                "message": {"type": "string"}
+            },
+            "required": ["status", "statusCode", "message"]
+        });
+        expect(validationResult.errors).toEqual([]);
+    }catch (e) {
+        throw processError(e);
+    }
 })
