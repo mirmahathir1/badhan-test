2f34a6129cc94c5f59aa147b015b096e15209704
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-15 13:06:18 +0600

    get/log/date/{date} done

---
+++ tests/logs/logByDate.test.js
@@ -0,0 +1,58 @@
+const {badhanAxios} = require('../../api');
+const validate = require('jsonschema').validate;
+const env = require('../../config/config');
+const {processError}=require('../fixtures/helpers');
+
+test('GET/log/statistics',async()=>{
+    try {
+        let signInResponse = await badhanAxios.post('/users/signin', {
+            phone: "8801521438557",
+            password: env.MAHATHIR_PASSWORD
+        });
+
+        const date=new Date().getTime();
+
+        let statisticsResponse = await badhanAxios.get('/log/date/'+date,{
+            headers:{
+                "x-auth":signInResponse.data.token
+            }
+        });
+
+
+        let validationResult = validate(statisticsResponse.data, {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                "status": {type: "string"},
+                "statusCode": {const: 200},
+                "message": {type: "string"},
+                "logs": {
+                    type:"array",
+                    items: {
+                        type:"object",
+                        additionalProperties: false,
+                        properties: {
+                            "donorId": {type: "string"},
+                            "hall": {type: "number"},
+                            "name": {type: "string"},
+                            "count":{type:"number"}
+                        },
+                        required: ["donorId", "hall", "name","count"]
+                    }
+                }
+            },
+            required: ["status", "statusCode", "message","logs"]
+        });
+
+        expect(validationResult.errors).toEqual([]);
+
+        await badhanAxios.delete('/users/signout', {
+            headers: {
+                "x-auth": signInResponse.data.token
+            }
+        });
+
+    }catch (e) {
+        throw processError(e);
+    }
+})
