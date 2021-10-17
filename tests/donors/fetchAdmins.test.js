42435f2a46589170f4729abd9a8312b31a6b654c
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-17 12:06:11 +0600

    get/olunteers/all done

---
+++ tests/donors/fetchAdmins.test.js
@@ -0,0 +1,83 @@
+const {badhanAxios} = require('../../api');
+const validate = require('jsonschema').validate;
+const env = require('../../config/config');
+const {processError} = require('../fixtures/helpers');
+
+test('GET/donors/me', async () => {
+    try {
+        let signInResponse = await badhanAxios.post('/users/signin', {
+            phone: "8801521438557",
+            password: env.MAHATHIR_PASSWORD
+        });
+
+        let adminResponse = await badhanAxios.get('/admins', {
+            headers: {
+                "x-auth": signInResponse.data.token
+            }
+        });
+
+
+        let validationResult = validate(adminResponse.data, {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                "status": {type: "string"},
+                "statusCode":{type:"number"},
+                "message":{type:"string"},
+                "admins": {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        additionalProperties: false,
+                        properties: {
+                            "address":{type:"string"},
+                            "roomNumber":{type:"string"},
+                            "designation":{type:"number"},
+                            "lastDonation":{type:"number"},
+                            "comment":{type:"string"},
+                            "commentTime":{type:"number"},
+                            "donationCount":{type:"number"},
+                            "email":{type:"string"},
+                            "_id": {type: "string"},
+                            "studentId": {type: "string"},
+                            "name": {type: "string"},
+                            "bloodGroup": {type: "number"},
+                            "phone": {type: "number"},
+                            "hall": {type: "number"},
+                            "availableToAll":{type:"boolean"},
+                            "donationCountOptimized":{type:"number"},
+                            "callRecords": {
+                                type: "array",
+                                items: {
+                                    type: "object",
+                                    additionalProperties: false,
+                                    properties: {
+                                        "_id": {type: "string"},
+                                        "callerId": {type: "string"},
+                                        "calleeId": {type: "string"},
+                                        "date": {type: "number"},
+                                        "expireAt": {type: "string"}
+                                    },
+                                    required: ["callerId", "calleeId","date", "expireAt"]
+                                }
+                            },
+                        },
+                        required: ["address","roomNumber","designation","lastDonation","comment","commentTime","donationCount", "email","_id", "studentId","name", "bloodGroup","hall", "phone","availableToAll","donationCountOptimized","callRecords"]
+                    },
+                },
+            },
+            required:["status","statusCode","message","admins"]
+        });
+
+        expect(validationResult.errors).toEqual([]);
+
+        await badhanAxios.delete('/users/signout', {
+            headers: {
+                "x-auth": signInResponse.data.token
+            }
+        });
+
+    } catch (e) {
+        throw processError(e);
+    }
+})
---
+++ tests/donors/fetchAllVolunteers.test.js
@@ -0,0 +1,56 @@
+const {badhanAxios} = require('../../api');
+const validate = require('jsonschema').validate;
+const env = require('../../config/config');
+const {processError} = require('../fixtures/helpers');
+
+test('GET/donors/me', async () => {
+    try {
+        let signInResponse = await badhanAxios.post('/users/signin', {
+            phone: "8801521438557",
+            password: env.MAHATHIR_PASSWORD
+        });
+
+        let allVolunteersResponse = await badhanAxios.get('/volunteers/all', {
+            headers: {
+                "x-auth": signInResponse.data.token
+            }
+        });
+
+        let validationResult = validate(allVolunteersResponse.data, {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                "status": {type: "string"},
+                "statusCode":{type:"number"},
+                "message":{type:"string"},
+                "data": {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        additionalProperties: false,
+                        properties: {
+                            "_id": {type: "string"},
+                            "studentId": {type: "string"},
+                            "name": {type: "string"},
+                            "logCount": {type: "number"},
+                            "hall": {type: "number"},
+                        },
+                        required: ["_id", "studentId","name", "logCount","hall"]
+                    },
+                },
+            },
+            required:["status","statusCode","message","data"]
+        });
+
+        expect(validationResult.errors).toEqual([]);
+
+        await badhanAxios.delete('/users/signout', {
+            headers: {
+                "x-auth": signInResponse.data.token
+            }
+        });
+
+    } catch (e) {
+        throw processError(e);
+    }
+})
