ed1acc65ae5c59bceca268f82de3b9b2006a655d
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-17 13:32:38 +0600

    get/donors/designation done

--- tests/donors/fetchDonorsDesignation.test.js
+++ tests/donors/fetchDonorsDesignation.test.js
@@ -3,7 +3,7 @@ const validate = require('jsonschema').validate;
 const env = require('../../config/config');
 const {processError} = require('../fixtures/helpers');
 
-test('GET/donors/designation', async() => {
+test('GET/donors/designation', async () => {
     try {
         let signInResponse = await badhanAxios.post('/users/signin', {
             phone: "8801521438557",
@@ -16,40 +16,61 @@ test('GET/donors/designation', async() => {
             }
         });
 
-        console.log(designationResponse.data);
-
         let validationResult = validate(designationResponse.data, {
             type: "object",
             additionalProperties: false,
             properties: {
                 "status": {type: "string"},
-                "statusCode":{const:200},
-                "message":{type:"string"},
-                "found":{type:"boolean"},
-                "donor": {
-                    type: "object",
-                    additionalProperties: false,
-                    properties: {
-                        "address":{type:"string"},
-                        "roomNumber":{type:"string"},
-                        "designation":{type:"number"},
-                        "lastDonation":{type:"number"},
-                        "comment":{type:"string"},
-                        "commentTime":{type:"number"},
-                        "donationCount":{type:"number"},
-                        "email":{type:"string"},
-                        "_id": {type: "string"},
-                        "studentId": {type: "string"},
-                        "phone": {type: "number"},
-                        "bloodGroup": {type: "number"},
-                        "hall": {type: "number"},
-                        "name": {type: "string"},
-                        "availableToAll":{type:"boolean"},
-                    },
-                    required: ["address","roomNumber","designation","lastDonation","comment","commentTime","donationCount", "email","_id", "studentId","phone","bloodGroup","hall","name","availableToAll"]
+                "statusCode": {const: 200},
+                "message": {type: "string"},
+                "volunteerList": {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        additionalProperties: false,
+                        properties: {
+                            "roomNumber": {type: "string"},
+                            "_id": {type: "string"},
+                            "studentId": {type: "string"},
+                            "phone": {type: "number"},
+                            "bloodGroup": {type: "number"},
+                            "name": {type: "string"},
+                        },
+                        required: ["roomNumber", "_id", "studentId", "phone", "bloodGroup", "name"]
+                    }
+                },
+                "adminList": {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        additionalProperties: false,
+                        properties: {
+                            "_id": {type: "string"},
+                            "studentId": {type: "string"},
+                            "phone": {type: "number"},
+                            "hall": {type: "number"},
+                            "name": {type: "string"},
+                        },
+                        required: [ "_id", "studentId", "phone", "hall", "name"]
+                    }
+                },
+                "superAdminList": {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        additionalProperties: false,
+                        properties: {
+                            "_id": {type: "string"},
+                            "studentId": {type: "string"},
+                            "phone": {type: "number"},
+                            "hall": {type: "number"},
+                            "name": {type: "string"},
+                        },
+                        required: [ "_id", "studentId", "phone", "hall", "name"]
+                    }
                 },
             },
-            required:["status","statusCode","message","donor"]
+            required: ["status", "statusCode", "message", "volunteerList","adminList","superAdminList"]
         });
 
         expect(validationResult.errors).toEqual([]);
