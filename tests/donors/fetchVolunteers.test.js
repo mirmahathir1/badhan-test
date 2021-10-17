338679ca659aec8b44dc9ad1caf988d3a7840b0e
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-17 12:19:18 +0600

    get/volunteers done

---
+++ tests/donors/fetchVolunteers.test.js
@@ -0,0 +1,59 @@
+const {badhanAxios} = require('../../api');
+const validate = require('jsonschema').validate;
+const env = require('../../config/config');
+const {processError} = require('../fixtures/helpers');
+
+test.skip('GET/donors/me', async () => {
+    try {
+        let signInResponse = await badhanAxios.post('/users/signin', {
+            phone: "8801521438557",
+            password: env.MAHATHIR_PASSWORD
+        });
+
+        let volunteersResponse = await badhanAxios.get('/volunteers', {
+            headers: {
+                "x-auth": signInResponse.data.token
+            }
+        });
+
+
+        let validationResult = validate(volunteersResponse.data, {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                "status": {type: "string"},
+                "statusCode":{type:"number"},
+                "message":{type:"string"},
+                "volunteerList": {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        additionalProperties: false,
+                        properties: {
+                            "roomNumber":{type:"string"},
+                            "_id": {type: "string"},
+                            "studentId": {type: "string"},
+                            "name": {type: "string"},
+                            "bloodGroup": {type: "number"},
+                            "phone": {type: "number"},
+                        },
+                        required: ["roomNumber","_id", "studentId","name", "bloodGroup","phone"]
+                    },
+                },
+            },
+            required:["status","statusCode","message","admins"]
+        });
+
+
+        // expect(validationResult.errors).toEqual([]);
+
+        await badhanAxios.delete('/users/signout', {
+            headers: {
+                "x-auth": signInResponse.data.token
+            }
+        });
+
+    } catch (e) {
+        throw processError(e);
+    }
+})
