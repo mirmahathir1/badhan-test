4d7e68a21ee3fb25451bec36ee90a97e50fb8d61
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-21 10:45:58 +0600

    done guest/users/signout

--- tests/users/signOut.test.js
+++ tests/users/signOut.test.js
@@ -2,6 +2,17 @@ const {badhanAxios} = require('../../api');
 const validate = require('jsonschema').validate;
 const env = require('../../config/config');
 const {processError}=require('../fixtures/helpers')
+const signOutSchema={
+    type: "object",
+    additionalProperties: false,
+    properties: {
+        status: {type: "string"},
+        statusCode: {const: 200},
+        message: {type: "string"}
+    },
+    required: ["status", "statusCode", "message"]
+}
+
 test('DELETE/users/signOut', async () => {
     try {
         let signInResponse = await badhanAxios.post('/users/signIn',{
@@ -12,16 +23,18 @@ test('DELETE/users/signOut', async () => {
                 "x-auth": signInResponse.data.token
             }
         });
-        let validationResult = validate(signOutResponse.data, {
-            type: "object",
-            additionalProperties: false,
-            properties: {
-                status: {type: "string"},
-                statusCode: {const: 200},
-                message: {type: "string"}
-            },
-            required: ["status", "statusCode", "message"]
-        });
+        let validationResult = validate(signOutResponse.data,signOutSchema);
+        expect(validationResult.errors).toEqual([]);
+    }catch (e) {
+        throw processError(e);
+    }
+})
+
+test('DELETE/guest/users/signOut', async () => {
+    try {
+
+        let signOutResponse = await badhanAxios.delete('/guest/users/signout');
+        let validationResult = validate(signOutResponse.data,signOutSchema);
         expect(validationResult.errors).toEqual([]);
     }catch (e) {
         throw processError(e);
