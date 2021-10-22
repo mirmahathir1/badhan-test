50a82dd276e6201b83324ff5ba13332991d78323
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-22 15:03:32 +0600

    done get/guest/log/statistics

--- tests/logs/logStatistics.test.js
+++ tests/logs/logStatistics.test.js
@@ -2,6 +2,26 @@ const {badhanAxios} = require('../../api');
 const validate = require('jsonschema').validate;
 const env = require('../../config/config');
 const {processError}=require('../fixtures/helpers');
+const statisticsSchema={
+    type: "object",
+    additionalProperties: false,
+    properties: {
+        status: {type: "string"},
+        statusCode: {const: 200},
+        message: {type: "string"},
+        statistics: {
+            type:"object",
+            additionalProperties:false,
+            properties:{
+                donorCount: {type:"number"},
+                donationCount: {type:"number"},
+                volunteerCount: {type:"number"}
+            },
+            required:["donorCount","donationCount","volunteerCount"]
+        }
+    },
+    required: ["status", "statusCode", "message","statistics"]
+}
 
 test('GET/log/statistics',async()=>{
     try {
@@ -17,26 +37,7 @@ test('GET/log/statistics',async()=>{
         });
 
 
-        let validationResult = validate(statisticsResponse.data, {
-            type: "object",
-            additionalProperties: false,
-            properties: {
-                status: {type: "string"},
-                statusCode: {const: 200},
-                message: {type: "string"},
-                statistics: {
-                    type:"object",
-                    additionalProperties:false,
-                    properties:{
-                        donorCount: {type:"number"},
-                        donationCount: {type:"number"},
-                        volunteerCount: {type:"number"}
-                    },
-                    required:["donorCount","donationCount","volunteerCount"]
-                }
-            },
-            required: ["status", "statusCode", "message","statistics"]
-        });
+        let validationResult = validate(statisticsResponse.data, statisticsSchema);
 
         expect(validationResult.errors).toEqual([]);
 
@@ -45,6 +46,19 @@ test('GET/log/statistics',async()=>{
                 "x-auth": signInResponse.data.token
             }
         });
+
+    }catch (e) {
+        throw processError(e);
+    }
+})
+
+test('GET/guest/log/statistics',async()=>{
+    try {
+        let statisticsResponse = await badhanAxios.get('/guest/log/statistics');
+
+        let validationResult = validate(statisticsResponse.data, statisticsSchema);
+
+        expect(validationResult.errors).toEqual([]);
 
     }catch (e) {
         throw processError(e);
