67ed62bc7e55687626eb83f894e21be0619d3a32
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-22 15:58:51 +0600

    post/guest/publicContacts

--- tests/publicContacts/contactsPostDelete.test.js
+++ tests/publicContacts/contactsPostDelete.test.js
@@ -2,6 +2,37 @@ const {badhanAxios} = require('../../api');
 const validate = require('jsonschema').validate;
 const env = require('../../config/config');
 const {processError} = require('../fixtures/helpers');
+const postPublicContactsSchema={
+    type: "object",
+    additionalProperties: false,
+    properties: {
+        status: {type: "string"},
+        statusCode: {const: 201},
+        message: {type: "string"},
+        publicContact: {
+            type: "object",
+            additionalProperties: false,
+            properties:{
+                bloodGroup:{type:"number"},
+                _id:{type:"string"},
+                donorId:{type:"string"}
+            },
+            required:["bloodGroup","_id","donorId"]
+        }
+    },
+    required: ["status", "statusCode", "message","publicContact"]
+}
+
+const deletePublicContactsSchema={
+    type: "object",
+    additionalProperties: false,
+    properties: {
+        status: {type: "string"},
+        statusCode: {const: 200},
+        message: {type: "string"}
+    },
+    required: ["status", "statusCode", "message"]
+}
 
 test('POST&DELETE/publicContacts', async () => {
     try {
@@ -22,26 +53,7 @@ test('POST&DELETE/publicContacts', async () => {
             }
         });
 
-        let validationContactResult = validate(contactCreationResponse.data, {
-            type: "object",
-            additionalProperties: false,
-            properties: {
-                status: {type: "string"},
-                statusCode: {const: 201},
-                message: {type: "string"},
-                publicContact: {
-                    type: "object",
-                    additionalProperties: false,
-                    properties:{
-                        bloodGroup:{type:"number"},
-                        _id:{type:"string"},
-                        donorId:{type:"string"}
-                    },
-                    required:["bloodGroup","_id","donorId"]
-                }
-            },
-            required: ["status", "statusCode", "message","publicContact"]
-        });
+        let validationContactResult = validate(contactCreationResponse.data, postPublicContactsSchema);
 
         expect(validationContactResult.errors).toEqual([]);
 
@@ -53,16 +65,7 @@ test('POST&DELETE/publicContacts', async () => {
             }
         });
 
-        let validationResult = validate(contactDeletionResponse.data, {
-            type: "object",
-            additionalProperties: false,
-            properties: {
-                status: {type: "string"},
-                statusCode: {const: 200},
-                message: {type: "string"}
-            },
-            required: ["status", "statusCode", "message"]
-        });
+        let validationResult = validate(contactDeletionResponse.data, deletePublicContactsSchema);
         expect(validationResult.errors).toEqual([]);
 
         await badhanAxios.delete('/users/signout', {
@@ -70,6 +73,29 @@ test('POST&DELETE/publicContacts', async () => {
                 "x-auth": signInResponse.data.token
             }
         });
+
+    } catch (e) {
+        throw processError(e);
+    }
+})
+
+test('POST&DELETE/guest/publicContacts', async () => {
+    try {
+
+        //post/callrecords part
+
+        let contactCreationResponse = await badhanAxios.post("/guest/publicContacts");
+
+        let validationContactResult = validate(contactCreationResponse.data, postPublicContactsSchema);
+
+        expect(validationContactResult.errors).toEqual([]);
+
+        // delete/donations part
+        //
+        // let contactDeletionResponse = await badhanAxios.delete("/guest/publicContacts?donorId="+env.SUPERADMIN_ID+"&contactId="+contactCreationResponse.data.publicContact["_id"]);
+        //
+        // let validationResult = validate(contactDeletionResponse.data, deletePublicContactsSchema);
+        // expect(validationResult.errors).toEqual([]);
 
     } catch (e) {
         throw processError(e);
