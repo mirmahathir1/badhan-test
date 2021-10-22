8f9d94dbc1975a48c9b36efca730e06581dab441
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-22 15:53:04 +0600

    get/guest/publicContacts

--- tests/publicContacts/fetchContacts.test.js
+++ tests/publicContacts/fetchContacts.test.js
@@ -1,45 +1,56 @@
 const {badhanAxios} = require('../../api');
 const validate = require('jsonschema').validate;
 const {processError}=require('../fixtures/helpers');
+const publicContactsSchema={
+    type: "object",
+    additionalProperties: false,
+    properties: {
+        status: {type: "string"},
+        statusCode: {const: 200},
+        message: {type: "string"},
+        publicContacts: {
+            type: "array",
+            items: {
+                type: "object",
+                additionalProperties: false,
+                properties: {
+                    bloodGroup:{type:"number"},
+                    contacts:{
+                        type:"array",
+                        items: {
+                            type:"object",
+                            additionalProperties:false,
+                            properties:{
+                                donorId:{type:"string"},
+                                phone:{type:"number"},
+                                name:{type:"string"},
+                                contactId:{type:"string"}
+                            },
+                            required:["donorId","phone","name","contactId"]
+                        }
+                    }
+                },
+                required:["bloodGroup","contacts"]
+            }
+        },
+    },
+    required: ["status", "statusCode", "message", "publicContacts"]
+}
 
 test('GET/publicContacts',async()=>{
     try {
         let contactResponse = await badhanAxios.get('/publicContacts');
-        let validationResult = validate(contactResponse.data, {
-            type: "object",
-            additionalProperties: false,
-            properties: {
-                status: {type: "string"},
-                statusCode: {const: 200},
-                message: {type: "string"},
-                publicContacts: {
-                    type: "array",
-                    items: {
-                        type: "object",
-                        additionalProperties: false,
-                        properties: {
-                            bloodGroup:{type:"number"},
-                            contacts:{
-                              type:"array",
-                              items: {
-                                  type:"object",
-                                  additionalProperties:false,
-                                  properties:{
-                                      donorId:{type:"string"},
-                                      phone:{type:"number"},
-                                      name:{type:"string"},
-                                      contactId:{type:"string"}
-                                  },
-                                  required:["donorId","phone","name","contactId"]
-                              }
-                            }
-                        },
-                        required:["bloodGroup","contacts"]
-                    }
-                },
-            },
-            required: ["status", "statusCode", "message", "publicContacts"]
-        });
+        let validationResult = validate(contactResponse.data, publicContactsSchema);
+        expect(validationResult.errors).toEqual([]);
+    }catch (e) {
+        throw processError(e);
+    }
+})
+
+test('GET/guest/publicContacts',async()=>{
+    try {
+        let contactResponse = await badhanAxios.get('/guest/publicContacts');
+        let validationResult = validate(contactResponse.data, publicContactsSchema);
         expect(validationResult.errors).toEqual([]);
     }catch (e) {
         throw processError(e);
