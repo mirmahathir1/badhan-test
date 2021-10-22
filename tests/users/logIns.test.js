66b4145881c1bb6c9a46576092cd19faa469eae1
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-22 23:22:46 +0600

    done get/guest/users/logins

--- tests/users/logIns.test.js
+++ tests/users/logIns.test.js
@@ -2,6 +2,41 @@ const {badhanAxios} = require('../../api');
 const validate = require('jsonschema').validate;
 const env = require('../../config/config');
 const {processError}=require('../fixtures/helpers');
+const logInsSchema={
+    type: "object",
+    additionalProperties: false,
+    properties: {
+        status: {type: "string"},
+        statusCode: {const: 200},
+        message: {type: "string"},
+        logins: {
+            type: "array",
+            items: {
+                types: "object",
+                additionalProperties: false,
+                properties: {
+                    _id: {type: "string"},
+                    os: {type: "string"},
+                    device: {type: "string"},
+                    browserFamily: {type: "string"},
+                    ipAddress: {type: "string"}
+                }
+            }
+        },
+        currentLogin: {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                _id: {type: "string"},
+                os: {type: "string"},
+                device: {type: "string"},
+                browserFamily: {type: "string"},
+                ipAddress: {type: "string"}
+            }
+        }
+    },
+    required: ["status", "statusCode", "message", "logins", "currentLogin"]
+}
 
 test('GET/users/logins',async()=>{
     try {
@@ -14,47 +49,23 @@ test('GET/users/logins',async()=>{
                 "x-auth": signInResponse.data.token
             }
         });
-        let validationResult = validate(logInsResponse.data, {
-            type: "object",
-            additionalProperties: false,
-            properties: {
-                status: {type: "string"},
-                statusCode: {const: 200},
-                message: {type: "string"},
-                logins: {
-                    type: "array",
-                    items: {
-                        types: "object",
-                        additionalProperties: false,
-                        properties: {
-                            _id: {type: "string"},
-                            os: {type: "string"},
-                            device: {type: "string"},
-                            browserFamily: {type: "string"},
-                            ipAddress: {type: "string"}
-                        }
-                    }
-                },
-                currentLogin: {
-                    type: "object",
-                    additionalProperties: false,
-                    properties: {
-                        _id: {type: "string"},
-                        os: {type: "string"},
-                        device: {type: "string"},
-                        browserFamily: {type: "string"},
-                        ipAddress: {type: "string"}
-                    }
-                }
-            },
-            required: ["status", "statusCode", "message", "logins", "currentLogin"]
-        });
+        let validationResult = validate(logInsResponse.data, logInsSchema);
         expect(validationResult.errors).toEqual([]);
         await badhanAxios.delete('/users/signout', {
             headers: {
                 "x-auth": signInResponse.data.token
             }
         });
+    }catch (e) {
+        throw processError(e);
+    }
+})
+
+test('GET/guest/users/logins',async()=>{
+    try {
+        let logInsResponse = await badhanAxios.get('/guest/users/logins');
+        let validationResult = validate(logInsResponse.data, logInsSchema);
+        expect(validationResult.errors).toEqual([]);
     }catch (e) {
         throw processError(e);
     }
