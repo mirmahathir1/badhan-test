01bca7c82f75dc9d25180f1fb37bb447bb191f1b
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-23 15:20:43 +0600

    modified fetchDonors

--- tests/donors/fetchDonors.test.js
+++ tests/donors/fetchDonors.test.js
@@ -2,6 +2,90 @@ const {badhanAxios} = require('../../api');
 const validate = require('jsonschema').validate;
 const env = require('../../config/config');
 const {processError} = require('../fixtures/helpers');
+const donorsSchema={
+    type: "object",
+    additionalProperties: false,
+    properties: {
+        status: {type: "string"},
+        statusCode: {const: 200},
+        message: {type: "string"},
+        donor: {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                _id:{type:"string"},
+                phone: {type: "number"},
+                name:{type:"string"},
+                studentId:{type:"string"},
+                email:{type:"string"},
+                lastDonation: {type: "number"},
+                bloodGroup: {type: "number"},
+                hall: {type: "number"},
+                roomNumber:{type:"string"},
+                address:{type:"string"},
+                comment:{type:"string"},
+                commentTime: {type: "number"},
+                designation: {type: "number"},
+                availableToAll: {type: "boolean"},
+                callRecords: {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        additionalProperties: false,
+                        properties: {
+                            _id: {type: "string"},
+                            callerId: {
+                                type: "object",
+                                additionalProperties:false,
+                                properties:{
+                                    designation:{type:"number"},
+                                    _id:{type:"string"},
+                                    name:{type:"string"},
+                                    hall:{type:"number"},
+                                },
+                                required:["designation","_id","name","hall"]
+                            },
+                            calleeId: {type: "string"},
+                            date: {type: "number"},
+                            expireAt: {type: "string"}
+                        },
+                        required: ["callerId", "calleeId","date", "expireAt"]
+                    }
+                },
+                donations: {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        additionalProperties: false,
+                        properties: {
+                            _id: {type: "string"},
+                            donorId: {type: "string"},
+                            phone: {type: "number"},
+                            date: {type: "number"}
+                        },
+                        required: ["_id", "donorId","phone", "date"]
+                    }
+                },
+                publicContacts: {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        additionalProperties: false,
+                        properties: {
+                            bloodGroup: {type: "number"},
+                            _id: {type: "string"},
+                            donorId: {type: "string"}
+                        },
+                        required: ["_id", "bloodGroup","donorId"]
+                    }
+                }
+            },
+            required: ["_id", "phone","name","studentId","email","lastDonation","bloodGroup","hall","roomNumber","address","comment","commentTime","designation","availableToAll","callRecords","donations","publicContacts"]
+        },
+    },
+    required: ["status", "statusCode", "message", "donor"]
+}
+
 
 test('GET/donors', async () => {
     try {
@@ -17,89 +101,8 @@ test('GET/donors', async () => {
         });
 
 
-        let validationResult = validate(donorsResponse.data, {
-            type: "object",
-            additionalProperties: false,
-            properties: {
-                status: {type: "string"},
-                statusCode: {const: 200},
-                message: {type: "string"},
-                donor: {
-                    type: "object",
-                    additionalProperties: false,
-                    properties: {
-                        _id:{type:"string"},
-                        phone: {type: "number"},
-                        name:{type:"string"},
-                        studentId:{type:"string"},
-                        email:{type:"string"},
-                        lastDonation: {type: "number"},
-                        bloodGroup: {type: "number"},
-                        hall: {type: "number"},
-                        roomNumber:{type:"string"},
-                        address:{type:"string"},
-                        comment:{type:"string"},
-                        commentTime: {type: "number"},
-                        designation: {type: "number"},
-                        availableToAll: {type: "boolean"},
-                        callRecords: {
-                            type: "array",
-                            items: {
-                                type: "object",
-                                additionalProperties: false,
-                                properties: {
-                                    _id: {type: "string"},
-                                    callerId: {
-                                        type: "object",
-                                        additionalProperties:false,
-                                        properties:{
-                                            designation:{type:"number"},
-                                            _id:{type:"string"},
-                                            name:{type:"string"},
-                                            hall:{type:"number"},
-                                        },
-                                        required:["designation","_id","name","hall"]
-                                    },
-                                    calleeId: {type: "string"},
-                                    date: {type: "number"},
-                                    expireAt: {type: "string"}
-                                },
-                                required: ["callerId", "calleeId","date", "expireAt"]
-                            }
-                        },
-                        donations: {
-                            type: "array",
-                            items: {
-                                type: "object",
-                                additionalProperties: false,
-                                properties: {
-                                    _id: {type: "string"},
-                                    donorId: {type: "string"},
-                                    phone: {type: "number"},
-                                    date: {type: "number"}
-                                },
-                                required: ["_id", "donorId","phone", "date"]
-                            }
-                        },
-                        publicContacts: {
-                            type: "array",
-                            items: {
-                                type: "object",
-                                additionalProperties: false,
-                                properties: {
-                                    bloodGroup: {type: "number"},
-                                    _id: {type: "string"},
-                                    donorId: {type: "string"}
-                                },
-                                required: ["_id", "bloodGroup","donorId"]
-                            }
-                        }
-                    },
-                    required: ["_id", "phone","name","studentId","email","lastDonation","bloodGroup","hall","roomNumber","address","comment","commentTime","designation","availableToAll","callRecords","donations","publicContacts"]
-                },
-            },
-            required: ["status", "statusCode", "message", "donor"]
-        });
+        let validationResult = validate(donorsResponse.data, donorsSchema);
+
         expect(validationResult.errors).toEqual([]);
         await badhanAxios.delete('/users/signout', {
             headers: {
