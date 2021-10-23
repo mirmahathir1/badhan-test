e163be1f8e9f992242ac5a1b27cad7be84c53695
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-23 15:32:43 +0600

    modified fetchDonorsDesignation

--- tests/donors/fetchDonorsDesignation.test.js
+++ tests/donors/fetchDonorsDesignation.test.js
@@ -2,6 +2,62 @@ const {badhanAxios} = require('../../api');
 const validate = require('jsonschema').validate;
 const env = require('../../config/config');
 const {processError} = require('../fixtures/helpers');
+const designationSchema={
+    type: "object",
+    additionalProperties: false,
+    properties: {
+        status: {type: "string"},
+        statusCode: {const: 200},
+        message: {type: "string"},
+        volunteerList: {
+            type: "array",
+            items: {
+                type: "object",
+                additionalProperties: false,
+                properties: {
+                    roomNumber: {type: "string"},
+                    _id: {type: "string"},
+                    studentId: {type: "string"},
+                    phone: {type: "number"},
+                    bloodGroup: {type: "number"},
+                    name: {type: "string"},
+                },
+                required: ["roomNumber", "_id", "studentId", "phone", "bloodGroup", "name"]
+            }
+        },
+        adminList: {
+            type: "array",
+            items: {
+                type: "object",
+                additionalProperties: false,
+                properties: {
+                    _id: {type: "string"},
+                    studentId: {type: "string"},
+                    phone: {type: "number"},
+                    hall: {type: "number"},
+                    name: {type: "string"},
+                },
+                required: [ "_id", "studentId", "phone", "hall", "name"]
+            }
+        },
+        superAdminList: {
+            type: "array",
+            items: {
+                type: "object",
+                additionalProperties: false,
+                properties: {
+                    _id: {type: "string"},
+                    studentId: {type: "string"},
+                    phone: {type: "number"},
+                    hall: {type: "number"},
+                    name: {type: "string"},
+                },
+                required: [ "_id", "studentId", "phone", "hall", "name"]
+            }
+        },
+    },
+    required: ["status", "statusCode", "message", "volunteerList","adminList","superAdminList"]
+}
 
 test('GET/donors/designation', async () => {
     try {
@@ -16,62 +72,7 @@ test('GET/donors/designation', async () => {
             }
         });
 
-        let validationResult = validate(designationResponse.data, {
-            type: "object",
-            additionalProperties: false,
-            properties: {
-                status: {type: "string"},
-                statusCode: {const: 200},
-                message: {type: "string"},
-                volunteerList: {
-                    type: "array",
-                    items: {
-                        type: "object",
-                        additionalProperties: false,
-                        properties: {
-                            roomNumber: {type: "string"},
-                            _id: {type: "string"},
-                            studentId: {type: "string"},
-                            phone: {type: "number"},
-                            bloodGroup: {type: "number"},
-                            name: {type: "string"},
-                        },
-                        required: ["roomNumber", "_id", "studentId", "phone", "bloodGroup", "name"]
-                    }
-                },
-                adminList: {
-                    type: "array",
-                    items: {
-                        type: "object",
-                        additionalProperties: false,
-                        properties: {
-                            _id: {type: "string"},
-                            studentId: {type: "string"},
-                            phone: {type: "number"},
-                            hall: {type: "number"},
-                            name: {type: "string"},
-                        },
-                        required: [ "_id", "studentId", "phone", "hall", "name"]
-                    }
-                },
-                superAdminList: {
-                    type: "array",
-                    items: {
-                        type: "object",
-                        additionalProperties: false,
-                        properties: {
-                            _id: {type: "string"},
-                            studentId: {type: "string"},
-                            phone: {type: "number"},
-                            hall: {type: "number"},
-                            name: {type: "string"},
-                        },
-                        required: [ "_id", "studentId", "phone", "hall", "name"]
-                    }
-                },
-            },
-            required: ["status", "statusCode", "message", "volunteerList","adminList","superAdminList"]
-        });
+        let validationResult = validate(designationResponse.data, designationSchema);
 
         expect(validationResult.errors).toEqual([]);
 
