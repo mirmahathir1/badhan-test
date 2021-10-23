4823ea77d13405cb7c0994906baa4731021bac53
sumaiyaazad <azadsumaiya00@gmail.com>
2021-10-23 15:56:11 +0600

    done get/guest/donors/checkDuplicate

--- tests/donors/fetchDuplicateDonor.test.js
+++ tests/donors/fetchDuplicateDonor.test.js
@@ -2,6 +2,38 @@ const {badhanAxios} = require('../../api');
 const validate = require('jsonschema').validate;
 const env = require('../../config/config');
 const {processError} = require('../fixtures/helpers');
+const duplicateDonorSchema={
+    type: "object",
+    additionalProperties: false,
+    properties: {
+        status: {type: "string"},
+        statusCode:{const:200},
+        message:{type:"string"},
+        found:{type:"boolean"},
+        donor: {
+            type: "object" ,
+            additionalProperties: false,
+            properties: {
+                address:{type:"string"},
+                roomNumber:{type:"string"},
+                designation:{type:"number"},
+                lastDonation:{type:"number"},
+                comment:{type:"string"},
+                commentTime:{type:"number"},
+                email:{type:"string"},
+                _id: {type: "string"},
+                studentId: {type: "string"},
+                phone: {type: "number"},
+                bloodGroup: {type: "number"},
+                hall: {type: "number"},
+                name: {type: "string"},
+                availableToAll:{type:"boolean"},
+            },
+            required: ["address","roomNumber","designation","lastDonation","comment","commentTime", "email","_id", "studentId","phone","bloodGroup","hall","name","availableToAll"]
+        },
+    },
+    required: ["status","statusCode","message","donor","found"]
+}
 
 test('GET/donors/checkDuplicate', async() => {
     try {
@@ -16,38 +48,7 @@ test('GET/donors/checkDuplicate', async() => {
             }
         });
 
-        let validationResult = validate(duplicateResponse.data, {
-            type: "object",
-            additionalProperties: false,
-            properties: {
-                status: {type: "string"},
-                statusCode:{const:200},
-                message:{type:"string"},
-                found:{type:"boolean"},
-                donor: {
-                        type: "object",
-                        additionalProperties: false,
-                        properties: {
-                            address:{type:"string"},
-                            roomNumber:{type:"string"},
-                            designation:{type:"number"},
-                            lastDonation:{type:"number"},
-                            comment:{type:"string"},
-                            commentTime:{type:"number"},
-                            email:{type:"string"},
-                            _id: {type: "string"},
-                            studentId: {type: "string"},
-                            phone: {type: "number"},
-                            bloodGroup: {type: "number"},
-                            hall: {type: "number"},
-                            name: {type: "string"},
-                            availableToAll:{type:"boolean"},
-                        },
-                        required: ["address","roomNumber","designation","lastDonation","comment","commentTime", "email","_id", "studentId","phone","bloodGroup","hall","name","availableToAll"]
-                    },
-                },
-            required:["status","statusCode","message","donor","found"]
-        });
+        let validationResult = validate(duplicateResponse.data, duplicateDonorSchema);
 
         expect(validationResult.errors).toEqual([]);
 
@@ -56,6 +57,19 @@ test('GET/donors/checkDuplicate', async() => {
                 "x-auth": signInResponse.data.token
             }
         });
+
+    } catch (e) {
+        throw processError(e);
+    }
+})
+
+test('GET/guest/donors/checkDuplicate', async() => {
+    try {
+        let duplicateResponse = await badhanAxios.get('/guest/donors/checkDuplicate?phone=8801521438557');
+
+        let validationResult = validate(duplicateResponse.data, duplicateDonorSchema);
+
+        expect(validationResult.errors).toEqual([]);
 
     } catch (e) {
         throw processError(e);
