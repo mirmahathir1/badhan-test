438277aa7861e0b43769635d6598a604487c072b
Mir Mahathir Mohammad <mirmahathir1@gmail.com>
2021-11-23 15:45:25 +0600

    updated testin for get /search/v3

--- tests/donors/searchDonors.test.js
+++ tests/donors/searchDonors.test.js
@@ -18,11 +18,9 @@ const searchSchema = {
                 properties: {
                     address: {type: "string"},
                     roomNumber: {type: "string"},
-                    designation: {type: "number"},
                     lastDonation: {type: "number"},
                     comment: {type: "string"},
                     commentTime: {type: "number"},
-                    email: {type: "string"},
                     _id: {type: "string"},
                     studentId: {type: "string"},
                     name: {type: "string"},
@@ -30,65 +28,45 @@ const searchSchema = {
                     phone: {type: "number"},
                     hall: {type: "number"},
                     availableToAll: {type: "boolean"},
-                    donationCountOptimized: {type: "number"},
-                    callRecords: {
-                        type: "array",
-                        // minItems: 1,
-                        items: {
-                            type: "object",
-                            additionalProperties: false,
-                            properties: {
-                                _id: {type: "string"},
-                                callerId: {type: "string"},
-                                calleeId: {type: "string"},
-                                date: {type: "number"},
-                            },
-                            required: ["callerId", "calleeId", "date", "_id"]
-                        }
-                    },
-                    markedBy: {
+                    donationCount: {type: "number"},
+                    callRecordCount: {type: "number"},
+                    marker: {
                         type:{
                             anyOf: [
                                 {
                                     type: "object",
                                     additionalProperties: false,
                                     properties: {
-                                        markerId:{
-                                            type:"object",
-                                            additionalProperties: false,
-                                            required: ["_id", "name"],
-                                            properties: {
-                                                _id:{type:"string"},
-                                                name:{type:"name"},
-                                            }
-                                        },
-                                        donorId:{type:"string",},
+                                        name:{type:"string",},
                                         time: {type: "number"},
                                     },
-                                    required: ["markerId", "time","donorId"]
+                                    required: ["name", "time"]
                                 },
                                 {
-                                    type: "null",
+                                    type: "object",
+                                    additionalProperties: false,
+                                    properties: {
+                                    },
                                 }
                             ]
                         },
                     }
                 },
-                required: ["address", "roomNumber", "designation", "lastDonation", "comment", "commentTime", "email", "_id", "studentId", "name", "bloodGroup", "hall", "phone", "availableToAll", "donationCountOptimized", "callRecords", "markedBy"]
+                required: ["address", "roomNumber", "lastDonation", "comment", "commentTime", "_id", "studentId", "name", "bloodGroup", "hall", "phone", "availableToAll", "donationCount", "callRecordCount", "marker"]
             },
         },
     },
     required: ["status", "statusCode", "message", "filteredDonors"]
 }
 
-test('GET/search/v2', async () => {
+test('GET/search/v3', async () => {
     try {
         let signInResponse = await badhanAxios.post('/users/signin', {
             phone: env.SUPERADMIN_PHONE,
             password: env.SUPERADMIN_PASSWORD
         });
 
-        let searchResponse = await badhanAxios.get('/search/v2?bloodGroup=2&hall=5&batch=16&name=maha&address=&isAvailable=true&isNotAvailable=true&availableToAll=true', {
+        let searchResponse = await badhanAxios.get('/search/v3?bloodGroup=2&hall=5&batch=16&name=maha&address=&isAvailable=true&isNotAvailable=true&availableToAll=true', {
             headers: {
                 "x-auth": signInResponse.data.token
             }
@@ -109,9 +87,9 @@ test('GET/search/v2', async () => {
     }
 })
 
-test('GET/guest/search/v2', async () => {
+test('GET/guest/search/v3', async () => {
     try {
-        let searchResponse = await badhanAxios.get('/guest/search/v2?bloodGroup=2&hall=5&batch=16&name=mahathir&address=&isAvailable=true&isNotAvailable=true&availableToAll=true');
+        let searchResponse = await badhanAxios.get('/guest/search/v3?bloodGroup=2&hall=5&batch=16&name=mahathir&address=&isAvailable=true&isNotAvailable=true&availableToAll=true');
 
         let validationResult = validate(searchResponse.data, searchSchema);
 
