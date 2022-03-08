986e1432efcfcb9d9dec14f616356b77a44d2634
Mir Mahathir Mohammad <36355905+mirmahathir1@users.noreply.github.com>
2022-03-08 15:47:38 +0600

    added testing for new route get /donors/checkDuplicate/many

--- config/index.js
+++ config/index.js
@@ -2,7 +2,12 @@ const config = require('../config/config')
 const env = {
     SUPERADMIN_ID: config.SUPERADMIN_ID,
     SUPERADMIN_PASSWORD: config.SUPERADMIN_PASSWORD,
-    SUPERADMIN_PHONE: config.SUPERADMIN_PHONE
+    SUPERADMIN_PHONE: config.SUPERADMIN_PHONE,
+    SUPERADMIN_HALL: config.SUPERADMIN_HALL,
+    VOLUNTEER_PHONE: config.VOLUNTEER_PHONE,
+    VOLUNTEER_PASSWORD: config.VOLUNTEER_PASSWORD,
+    VOLUNTEER_ID: config.VOLUNTEER_ID,
+    VOLUNTEER_HALL: config.VOLUNTEER_HALL
 }
 Object.keys(env).forEach((key) => {
     if (env[key] === undefined) {
---
+++ tests/donors/fetchDonorsDuplicateAll.test.js
@@ -0,0 +1,121 @@
+const {badhanAxios} = require('../../api');
+const validate = require('jsonschema').validate;
+const env = require('../../config');
+const {processError} = require('../fixtures/helpers');
+
+const duplicateDonorsManySchema = {
+    type: "object",
+    additionalProperties: false,
+    properties: {
+        status: {type: "string"},
+        statusCode: {const: 200},
+        message: {type: "string"},
+        donors: {
+            type: "array",
+            minItems: 10,
+            items: {
+                type: "object",
+                additionalProperties: false,
+                properties: {
+                    donorId: {
+                        "anyOf":[{
+                            type: "string", minLength: 24, maxLength: 24
+                        },{
+                            const : "FORBIDDEN"
+                        }]},
+                    phone: {type: "number"}
+                },
+                required: ["donorId","phone"]
+            },
+        },
+    },
+    required: ["status", "statusCode", "message", "donors"]
+}
+
+test('GET/donors/checkDuplicate/many',async()=>{
+    try{
+        let superAdminSignInResponse = await badhanAxios.post('/users/signin', {
+            phone: env.SUPERADMIN_PHONE,
+            password: env.SUPERADMIN_PASSWORD
+        });
+        let volunteerSignInResponse = await badhanAxios.post('/users/signin',{
+            phone: env.VOLUNTEER_PHONE,
+            password: env.VOLUNTEER_PASSWORD,
+        })
+
+        let searchQuery
+        searchQuery = {
+            bloodGroup: 2,
+            hall: env.VOLUNTEER_HALL,
+            batch: '',
+            name: '',
+            address: '',
+            isAvailable: true,
+            isNotAvailable: true,
+            availableToAll: false,
+        }
+
+        let superAdminSearchResultsForVolunteersHall = await badhanAxios.get(`/search/v3?bloodGroup=${searchQuery.bloodGroup}&hall=${searchQuery.hall}&batch=${searchQuery.batch}&name=${searchQuery.name}&address=${searchQuery.address}&isAvailable=${searchQuery.isAvailable}&isNotAvailable=${searchQuery.isNotAvailable}&availableToAll=${searchQuery.availableToAll}`,{
+            headers: {
+                "x-auth": superAdminSignInResponse.data.token
+            }
+        })
+
+        expect(superAdminSearchResultsForVolunteersHall.data.filteredDonors.length).toBeGreaterThanOrEqual(5)
+
+        let hallThatIsNotOfVolunteer
+        for(hallThatIsNotOfVolunteer = 0; hallThatIsNotOfVolunteer <7; hallThatIsNotOfVolunteer++){
+            if(hallThatIsNotOfVolunteer!==env.VOLUNTEER_HALL){
+                break
+            }
+        }
+
+        searchQuery = {
+            bloodGroup: 2,
+            hall: hallThatIsNotOfVolunteer,
+            batch: '',
+            name: '',
+            address: '',
+            isAvailable: true,
+            isNotAvailable: true,
+            availableToAll: false,
+        }
+
+        let superAdminSearchResultsForOtherHall = await badhanAxios.get(`/search/v3?bloodGroup=${searchQuery.bloodGroup}&hall=${searchQuery.hall}&batch=${searchQuery.batch}&name=${searchQuery.name}&address=${searchQuery.address}&isAvailable=${searchQuery.isAvailable}&isNotAvailable=${searchQuery.isNotAvailable}&availableToAll=${searchQuery.availableToAll}`,{
+            headers: {
+                "x-auth": superAdminSignInResponse.data.token
+            }
+        })
+
+        expect(superAdminSearchResultsForOtherHall.data.filteredDonors.length).toBeGreaterThanOrEqual(5)
+
+        let listOfPhones = []
+        listOfPhones.push(...superAdminSearchResultsForOtherHall.data.filteredDonors.map(donor=>donor.phone))
+        listOfPhones.push(...superAdminSearchResultsForVolunteersHall.data.filteredDonors.map(donor=>donor.phone))
+
+        let phoneListQuery = '?phoneList='+listOfPhones.join('&phoneList=')
+        let existingDonorsResponse = await badhanAxios.get(`/donors/checkDuplicate/many${phoneListQuery}`,{
+            headers: {
+                "x-auth": volunteerSignInResponse.data.token
+            }
+        })
+        let existingDonorValidationResult = validate(existingDonorsResponse.data, duplicateDonorsManySchema)
+
+        expect(existingDonorValidationResult.errors).toEqual([]);
+
+        await badhanAxios.delete('/users/signout', {
+            headers: {
+                "x-auth": volunteerSignInResponse.data.token
+            }
+        });
+        await badhanAxios.delete('/users/signout', {
+            headers: {
+                "x-auth": superAdminSignInResponse.data.token
+            }
+        });
+
+
+    } catch (e) {
+        throw processError(e);
+    }
+})
