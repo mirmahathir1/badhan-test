d04c7e0b7d969055bbf41ccc4912f44640989afe
Mir Mahathir Mohammad <36355905+mirmahathir1@users.noreply.github.com>
2022-03-27 22:58:36 +0600

    added testing of guest route of get /log/version/v5

--- tests/logs/logVersion.test.js
+++ tests/logs/logVersion.test.js
@@ -25,3 +25,27 @@ test('GET/log/version/v5',async()=>{
         throw processError(e);
     }
 })
+
+test('GET/guest/log/version/v5',async()=>{
+    try {
+
+        let logVersionResponse = await badhanAxios.get('/guest/log/version/v5');
+        let validationResult = validate(logVersionResponse.data, {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                status: {type: "string"},
+                statusCode: {const: 200},
+                message: {type: "string"},
+                githubReleaseVersion :{type:"string"},
+                githubReleaseDownloadURL :{type:"string"},
+                firebaseVersion :{type:"string"}
+            },
+            required: ["status", "statusCode", "message", "githubReleaseVersion", "githubReleaseDownloadURL", "firebaseVersion"]
+        });
+        expect(validationResult.errors).toEqual([]);
+
+    }catch (e) {
+        throw processError(e);
+    }
+})
