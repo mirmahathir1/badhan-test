e1e136f6548e251dea6cfc4152fb271612ced373
Mir Mahathir Mohammad <36355905+mirmahathir1@users.noreply.github.com>
2023-07-09 13:56:14 +0600

    fix activeDonors test for random db

--- tests/activeDonors/activeDonorsPostDelete.test.js
+++ tests/activeDonors/activeDonorsPostDelete.test.js
@@ -44,6 +44,50 @@ const deleteActiveDonorSchema = {
     }
 }
 
+const activeDonorSearchResultSchema = {
+    type: "object",
+    additionalProperties: false,
+    properties: {
+        status: {type: "string"},
+        statusCode: {const: 200},
+        message: {type: "string"},
+        activeDonors: {
+            type: "array",
+            minItems: 1,
+            items: {
+                type: "object",
+                additionalProperties: false,
+                properties: {
+                    _id: {type: "string"},
+                    hall: {type: "integer"},
+                    name: {type: "string"},
+                    address: {type: "string"},
+                    comment: {type: "string"},
+                    commentTime: {type: "integer"},
+                    lastDonation: {type: "integer"},
+                    availableToAll: {type: "boolean"},
+                    bloodGroup: {type: "integer"},
+                    studentId: {type: "string"},
+                    phone: {type: "integer"},
+                    markedTime: {type: "integer"},
+                    markerName: {type: "string"},
+                    donationCount: {type: "integer"},
+                    callRecordCount: {type: "integer"},
+                    lastCallRecord: {
+                        type: {
+                            anyOf: [{type: "integer"},{type:"null"}],
+                        }
+                    }
+                },
+                required: [
+                    "_id", "hall", "name", "address", "comment", "commentTime", "lastDonation", "availableToAll", "bloodGroup", "studentId", "phone", "markedTime", "markerName", "donationCount", "callRecordCount", "lastCallRecord",
+                ]
+            }
+        }
+    },
+    required: ["status", "statusCode", "message", "activeDonors"]
+}
+
 test('POST & DELETE /activeDonors',async()=>{
     try {
         let signInResponse = await badhanAxios.post('/users/signin', {
@@ -51,9 +95,15 @@ test('POST & DELETE /activeDonors',async()=>{
             password: env.SUPERADMIN_PASSWORD
         });
 
+        let donorResponse = await badhanAxios.get('/users/me', {
+            headers: {
+                "x-auth": signInResponse.data.token
+            }
+        });
+
         // clear up any existing active donor to begin testing
         try{
-            await badhanAxios.delete(`/activeDonors/${env.SUPERADMIN_ID}`, {
+            await badhanAxios.delete(`/activeDonors/${donorResponse.data.donor._id}`, {
                 headers: {
                     "x-auth": signInResponse.data.token
                 }
@@ -63,7 +113,7 @@ test('POST & DELETE /activeDonors',async()=>{
         }
 
         let createActiveDonorResponse = await badhanAxios.post('/activeDonors',{
-            donorId: env.SUPERADMIN_ID,
+            donorId: donorResponse.data.donor._id,
         },{
             headers: {
                 "x-auth": signInResponse.data.token
@@ -74,7 +124,17 @@ test('POST & DELETE /activeDonors',async()=>{
 
         expect(createActiveDonorValidationResult.errors).toEqual([]);
 
-        let deleteActiveDonorResponse = await badhanAxios.delete(`/activeDonors/${env.SUPERADMIN_ID}`, {
+        let getActiveDonorResponse = await badhanAxios.get(`/activeDonors?bloodGroup=-1&hall=${donorResponse.data.donor.hall}&batch=&name=&address=&isAvailable=true&isNotAvailable=true&availableToAll=false&markedByMe=false&availableToAllOrHall=false`, {
+            headers: {
+                "x-auth": signInResponse.data.token
+            }
+        });
+
+        let activeDonorSearchValidationResult = validate(getActiveDonorResponse.data, activeDonorSearchResultSchema);
+
+        expect(activeDonorSearchValidationResult.errors).toEqual([]);
+
+        let deleteActiveDonorResponse = await badhanAxios.delete(`/activeDonors/${donorResponse.data.donor._id}`, {
             headers: {
                 "x-auth": signInResponse.data.token
             }
@@ -102,7 +162,7 @@ test('POST & DELETE /guest/activeDonors',async()=>{
 
         expect(createActiveDonorValidationResult.errors).toEqual([]);
 
-        let deleteActiveDonorResponse = await badhanAxios.delete(`/guest/activeDonors/${env.SUPERADMIN_ID}`, {
+        let deleteActiveDonorResponse = await badhanAxios.delete(`/guest/activeDonors/123456`, {
         });
 
         let deleteActiveDonorValidateResult = validate(deleteActiveDonorResponse.data,deleteActiveDonorSchema);
